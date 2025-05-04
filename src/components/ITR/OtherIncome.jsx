df0c2933f416d1668f03e0bea1b243b58e6adf1a
vikasgaddam9010 <vikasgaddam9010@gmail.com>
2025-05-04 19:16:30 +0530

    enhanced itr forms ui in each client page

--- src/components/ITR/OtherIncome.jsx
+++ src/components/ITR/OtherIncome.jsx
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 
+const EachSapn = ({children}) => {
+    return (
+        <span className="border-l border-t-0 border-b border-r-0 border-gray-300 !w-[200px] px-2 py-1 flex items-center justify-between "> {children}</span>
+    )
+}
+
+const options = ["Raw Material", "Products", 'Services', 'Grants', 'Donation', 'Other Operating Revenue', 'Revenue From Operations (Gross)', 'Excise Duty']
+
+
 const OtherIncome = () => {
     const [blocks, setBlocks] = useState([
     { id: 0, label: 'a', hasLabel: true, inputs: ['', ''] },
@@ -78,104 +87,187 @@ const OtherIncome = () => {
     }
     }, [blocksForOtherOperatingRevenue]);
 
+
+    const handleInputChange = (index, inputIndex, value, stateName, setter) => {
+        console.log(index)
+        const updatedBlocks = [...stateName];
+        updatedBlocks[index].inputs[inputIndex] = value;
+        setter(updatedBlocks);
+    
+        // Add new block if last one is fully filled
+        const isLastBlock = index === stateName.length - 1;
+        const isFilled = updatedBlocks[index].inputs.every((i) => i.trim() !== '');
+    
+        if (isLastBlock && isFilled) {
+        updatedBlocks.push({
+            id: updatedBlocks.length,
+            hasLabel: false,
+            inputs: ['', '',''],
+        });
+        setter([...updatedBlocks]);
+        }
+    
+        // Remove the next block if it's fully empty
+        const nextBlock = updatedBlocks[index + 1];
+        if (
+        nextBlock &&
+        nextBlock.inputs.every((input) => input.trim() === '') &&
+        !nextBlock.hasLabel
+        ) {
+        updatedBlocks.splice(index + 1, 1);
+        setter([...updatedBlocks]);
+        }
+    };
+
   return (
     <>
-    <div>
+    <div className="border border-x-0 border-b-0 border-gray-300">
+        <div className="flex">
+            <p className="border-b border-gray-300 pl-6 pr-4 py-1 w-full">(a) Dividend Income</p>
+            <div className=" flex">
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+
+            </div>
+        </div>
         {blocks.map((block, index) => (
-            <div
-            key={block.id}
-            className="pl-6 border border-t-0 flex items-center justify-between"
-            >
-            <p>
-                {block.hasLabel ? `(${block.label}) Interest Income` : ''}
-            </p>
-            <div>
+            <div key={block.id} className="w-full flex  justify-between">
                 <input
-                className="border-l !w-[250px]"
-                value={block.inputs[0]}
-                onChange={(e) => handleInputChange(index, 0, e.target.value, blocks, setBlocks)}
+                    className="pl-6 pr-4 py-1 w-full border-b border-gray-300"
+                    value={block.inputs[0]}
+                    onChange={(e) => handleInputChange(index, 0, e.target.value, blocks, setBlocks)}
                 />
-                <input
-                className="border-l !w-[250px]"
-                value={block.inputs[1]}
-                onChange={(e) => handleInputChange(index, 1, e.target.value, blocks, setBlocks)}
-                />
-            </div>
+                <select className="min-w-[150px] outline-none border-l border-b  border-gray-300 ">
+                    {
+                        options.map((each, i) => <option key={i}>{each}</option>)
+                    }
+                </select>
+                <div className="flex">
+                    
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[1]}
+                        onChange={(e) => handleInputChange(index, 1, e.target.value, blocks, setBlocks)}
+                    />
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[2]}
+                        onChange={(e) => handleInputChange(index, 2, e.target.value, blocks, setBlocks)}
+                    />
+                </div>
             </div>
         ))}
     </div>
-    <div>
+    <div className="border border-x-0 border-b-0 border-gray-300">
+        <div className="flex">
+            <p className="border-b border-gray-300 pl-6 pr-4 py-1 w-full">(b) Net Gain on Sale of Invetments</p>
+            <div className=" flex">
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+
+            </div>
+        </div>
         {blocksForSaleOfServices.map((block, index) => (
-            <div
-            key={block.id}
-            className="pl-6 border border-t-0 flex items-center justify-between"
-            >
-            <p>
-                {block.hasLabel ? `(${block.label}) Dividend Income` : ''}
-            </p>
-            <div>
+            <div key={block.id} className="w-full flex  justify-between">
                 <input
-                className="border-l !w-[250px]"
-                value={block.inputs[0]}
-                onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
+                    className="pl-6 pr-4 py-1 w-full border-b border-gray-300"
+                    value={block.inputs[0]}
+                    onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
                 />
-                <input
-                className="border-l !w-[250px]"
-                value={block.inputs[1]}
-                onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
-                />
-            </div>
-            </div>
-        ))}
-    </div>
-    <div>
-        {blocksForSaleOfServices.map((block, index) => (
-            <div
-            key={block.id}
-            className="pl-6 border border-t-0 flex items-center justify-between"
-            >
-            <p>
-                {block.hasLabel ? `(${block.label}) Net Gain on Sale of Invetments` : ''}
-            </p>
-            <div>
-                <input
-                className="border-l !w-[250px]"
-                value={block.inputs[0]}
-                onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
-                />
-                <input
-                className="border-l !w-[250px]"
-                value={block.inputs[1]}
-                onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
-                />
-            </div>
+                <select className="min-w-[150px] outline-none border-l border-b  border-gray-300 ">
+                    {
+                        options.map((each, i) => <option key={i}>{each}</option>)
+                    }
+                </select>
+                <div className="flex">
+                    
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[1]}
+                        onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
+                    />
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[2]}
+                        onChange={(e) => handleInputChange(index, 2, e.target.value, blocksForSaleOfServices, setBlocksForSaleOfServices)}
+                    />
+                </div>
             </div>
         ))}
     </div>
-    <div>
+    <div className="border border-x-0 border-b-0 border-gray-300">
+                <div className="flex">
+                    <p className="border-b border-gray-300 pl-6 pr-4 py-1 w-full">(c) Net Gain on Sale of Investments</p>
+                    <div className="flex">
+                        <EachSapn><span>Total Grants in Rs.</span> <strong>0/-</strong></EachSapn>
+                        <EachSapn><span>Total Grants in Rs.</span> <strong>0/-</strong></EachSapn>
+                    </div>
+                </div>
+                {blocksForGrantsOrDonationReceived.map((block, index) => (
+                    <div key={block.id} className="w-full flex  justify-between">
+                        <input
+                            className="pl-6 pr-4 py-1 w-full border-b border-gray-300"
+                            value={block.inputs[0]}
+                            onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                        />
+                        <select className="min-w-[150px] outline-none border-l border-b  border-gray-300 ">
+                            {
+                                options.map((each, i) => <option key={i}>{each}</option>)
+                            }
+                        </select>
+                        <div className="flex">
+                            <input
+                                className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                                value={block.inputs[1]}
+                                onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                            />
+                            <input
+                                className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                                value={block.inputs[2]}
+                                onChange={(e) => handleInputChange(index, 2, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+    <div className="border border-x-0 border-b-0 border-gray-300">
+        <div className="flex">
+            <p className="border-b border-gray-300 pl-6 pr-4 py-1 w-full">(d) Other Non-Operating Income (Please Specify)</p>
+            <div className=" flex">
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+                <EachSapn><span>Total Sales in Rs.</span> <strong>0/-</strong></EachSapn>
+
+            </div>
+        </div>
         {blocksForGrantsOrDonationReceived.map((block, index) => (
-            <div
-            key={block.id}
-            className="pl-6 border border-t-0 flex items-center justify-between"
-            >
-            <p>
-                {block.hasLabel ? `(${block.label}) Other Non-Operating Income (Please Specify)` : ''}
-            </p>
-            <div>
+            <div key={block.id} className="w-full flex  justify-between">
                 <input
-                className="border-l !w-[250px]"
-                value={block.inputs[0]}
-                onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                    className="pl-6 pr-4 py-1 w-full border-b border-gray-300"
+                    value={block.inputs[0]}
+                    onChange={(e) => handleInputChange(index, 0, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
                 />
-                <input
-                className="border-l !w-[250px]"
-                value={block.inputs[1]}
-                onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
-                />
-            </div>
+                <select className="min-w-[150px] outline-none border-l border-b  border-gray-300 ">
+                    {
+                        options.map((each, i) => <option key={i}>{each}</option>)
+                    }
+                </select>
+                <div className="flex">
+                    
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[1]}
+                        onChange={(e) => handleInputChange(index, 1, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                    />
+                    <input
+                        className="border-l border-r-0 border-b border-t-0 border-gray-300 !w-[200px] px-4 py-1"
+                        value={block.inputs[2]}
+                        onChange={(e) => handleInputChange(index, 2, e.target.value, blocksForGrantsOrDonationReceived, setBlocksForGrantsOrDonationReceived)}
+                    />
+                </div>
             </div>
         ))}
     </div>
+    
     </>
   );
 }
--- src/components/ITR/eachClientData.jsx
+++ src/components/ITR/eachClientData.jsx
@@ -35,13 +35,13 @@ const data = {
     },
     'Firms Income': [],
     'Capital Gains': {
-      Shares: [{name:'LTCG', icon:<IoHome className="mx-3.5"/>}, {name:'STCG', icon:<IoHome className="mx-3.5"/>}],
-      'Other Assets': [{name:'LTCG', icon:<IoHome className="mx-3.5"/>}, {name:'STCG', icon:<IoHome className="mx-3.5"/>}],
+      Shares: [{name:'LTCG', icon:<IoHome className="mx-1"/>}, {name:'STCG', icon:<IoHome className="mx-1"/>}],
+      'Other Assets': [{name:'LTCG', icon:<IoHome className="mx-1"/>}, {name:'STCG', icon:<IoHome className="mx-1"/>}],
     },
-    ComputationTwo : [{name: 'Other Income', icon:<IoHome className="mx-3.5"/>},{name: 'Agriculture Income', icon:<IoHome className="mx-3.5"/>},
-      {name: 'Clubbing', icon:<IoHome className="mx-3.5"/>},{name: 'Setoff Losses', icon:<IoHome className="mx-3.5"/>},
-      {name: 'Gross Total Income', icon:<IoHome className="mx-3.5"/>},{name: 'Total Deductions', icon:<IoHome className="mx-3.5"/>},
-      {name: 'Net Income', icon:<IoHome className="mx-3.5"/>}],
+    ComputationTwo : [{name: 'Other Income', icon:<IoHome className="mx-1"/>},{name: 'Agriculture Income', icon:<IoHome className="mx-1"/>},
+      {name: 'Clubbing', icon:<IoHome className="mx-1"/>},{name: 'Setoff Losses', icon:<IoHome className="mx-1"/>},
+      {name: 'Gross Total Income', icon:<IoHome className="mx-1"/>},{name: 'Total Deductions', icon:<IoHome className="mx-1"/>},
+      {name: 'Net Income', icon:<IoHome className="mx-1"/>}],
     'Tax Thereon': ['Tax', 'Edu Cess'],
     'TDS/TCS/ADV Tax.': [],
     'Net Tax': ['Net Tax', 'Interest'],
@@ -161,7 +161,7 @@ const EachClientData = () => {
   };
 
   return (
-    <div className="rounded-md px-3 py-1 space-y-1">
+    <div className="rounded-md px-0.5 py-1 space-y-1">
       <div className="flex h-[18h] w-full">
         <EachHeading className="min-w-[60px] border-r-0">
           <img
@@ -193,7 +193,7 @@ const EachClientData = () => {
         Header row
         <div className=" flex font-bold">
           <div
-            className="px-3 flex items-center border transition-all duration-300"
+            className="px-0.5 flex items-center border transition-all duration-300"
             style={{ width: isOpen ? '269px' : `${62 + maxIndentLevel * 24.5}px` }}
           >
             <button
@@ -220,7 +220,7 @@ const EachClientData = () => {
         {filteredRows.map((row, idx) => (
           <div key={row.key} className=" flex  border-t-0">
             <div
-              className="px-3 flex items-center border-r border-l border-b transition-all duration-300"
+              className="px-0.5 flex items-center border-r border-l border-b transition-all duration-300"
               style={{ width: isOpen ? '269px' : `${62 + maxIndentLevel * 24}px` }}
             >
               <div
@@ -256,14 +256,14 @@ const EachClientData = () => {
       <div className="flex gap-5">
         <div>
           <div className=" flex font-bold">
-            <div className="px-3 flex items-center border transition-all duration-300">
+            <div className="px-0.5 flex items-center border transition-all duration-300">
               <button
                 onClick={toggleOpen}
-                className="mx-2 p-1 bg-blue-800 text-white rounded hover:bg-blue-600"
+                className="mx-0.5 p-1 bg-blue-800 text-white rounded hover:bg-blue-600"
               >
                 {isOpen ? <MdChevronLeft size={20} /> : <MdChevronRight size={20} />}
               </button>
-              <span className="w-4"></span>
+              <span className="w-2"></span>
 
               <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] transition-all ease-in-out duration-100`}>Asst. Year</span>
             </div>
@@ -283,8 +283,8 @@ const EachClientData = () => {
           }
           {selectedBtn === 'balance sheet' && <>
             <div className="transition-all  ease-in-out duration-100 flex font-bold">
-              <div onClick={() => {!accordion.balanceSheet && setSelectedBtn(""); setAccordion({...accordion, balanceSheet: !accordion.balanceSheet})}} className="cursor-pointer px-3 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
-                <IoHome className="mx-3.5"/>
+              <div onClick={() => {accordion.balanceSheet && setSelectedBtn(""); setAccordion({...accordion, balanceSheet: !accordion.balanceSheet})}} className="cursor-pointer px-0.5 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
+                <IoHome className="mx-1"/>
                   <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] flex gap-2 `}>Balance Sheet</span> 
                   {accordion.balanceSheet ? <IoArrowDown  /> : <IoArrowUp  />}
               </div>
@@ -293,8 +293,8 @@ const EachClientData = () => {
             </div>    
             {
               accordion?.balanceSheet && data?.BalanceSheet?.map((each, i) => <div key={i} className="transition-all ease-in-out duration-100 flex font-bold">
-                <div onClick={() => setRightContent(each)} className="cursor-pointer px-3 flex items-center border transition-all duration-300">
-                    <IoHome className="mx-3.5"/>
+                <div onClick={() => setRightContent(each)} className="cursor-pointer px-0.5 flex items-center border transition-all duration-300">
+                    <IoHome className="mx-1"/>
                     <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] font-normal   transition-all ease-in-out duration-100`}>{each}</span>
                     <span className="w-4"></span>
                 </div>
@@ -306,8 +306,8 @@ const EachClientData = () => {
           </>}
           {selectedBtn === 'audit  Reoort' && <>
             <div className="transition-all  ease-in-out duration-100 flex font-bold">
-              <div onClick={() => {!accordion.balanceSheet && setSelectedBtn(""); setAccordion({...accordion, balanceSheet: !accordion.balanceSheet})}} className="cursor-pointer px-3 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
-                <IoHome className="mx-3.5"/>
+              <div onClick={() => {!accordion.balanceSheet && setSelectedBtn(""); setAccordion({...accordion, balanceSheet: !accordion.balanceSheet})}} className="cursor-pointer px-0.5 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
+                <IoHome className="mx-1"/>
                   <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] flex gap-2 `}>Audit Reoort</span> 
                   <span className="w-4"></span>
               </div>
@@ -317,8 +317,8 @@ const EachClientData = () => {
           </>}
           {selectedBtn === 'online Activities' && <>
             <div className="transition-all  ease-in-out duration-100 flex font-bold">
-              <div className="cursor-pointer px-3 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
-                <IoHome className="mx-3.5"/>
+              <div className="cursor-pointer px-0.5 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
+                <IoHome className="mx-1"/>
                   <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] flex gap-2 `}>Online Activities</span> 
                   <span className="w-4"></span>
               </div>
@@ -327,8 +327,8 @@ const EachClientData = () => {
             </div>    
           </>}{selectedBtn === 'reports' && <>
             <div className="transition-all  ease-in-out duration-100 flex font-bold">
-              <div className="cursor-pointer px-3 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
-                <IoHome className="mx-3.5"/>
+              <div className="cursor-pointer px-0.5 flex items-center border bg-blue-50 text-blue-500 hover:text-blue-700">
+                <IoHome className="mx-1"/>
                   <span className={`${isOpen ? 'ml-2' : 'hidden'} w-[151px] flex gap-2 `}>Reports</span> 
                   <span className="w-4"></span>
               </div>
@@ -349,7 +349,7 @@ const EachClientData = () => {
             Reports</button>
           </div>
         </div>
-        <div className="w-full">
+        {selectedBtn === "balance sheet" && <div className="w-full">
           {
             rightContent === data?.BalanceSheet[0] && <div><h1 className="font-bold">{rightContent}</h1></div>
           }
@@ -359,50 +359,61 @@ const EachClientData = () => {
             rightContent === data?.BalanceSheet[2] && <div><h1 className="font-bold">{rightContent}</h1></div>
           }{
             rightContent === data?.BalanceSheet[3] && 
-            <div  className="w-full">
-              <div className="font-bold border border-b-0 flex items-center justify-between">
+            <div  className="w-full ">
+              <select className="w-full border rounded p-2 mb-2">
+                <option>Business 1</option>
+                <option>Business 1</option>
+                <option>Business 1</option>
+              </select>
+              <div className="font-bold border border-b-0 flex items-center justify-between   ">
                 <h1 className=" pl-2">{rightContent}</h1>
                 <div className="flex ">
-                  <p className="border-l  min-w-[250px] text-center pl-2 py-1.5" >31st March {currentYear}</p>
-                  <p className="border-l min-w-[250px] text-center">31st March {currentYear-1}</p>
+                <span className="border-l min-w-[150px] text-center pl-2 flex items-center justify-between px-2">Group</span>
+                  <p className="border-l min-w-[200px] text-center  py-1.5" >31st March {currentYear}</p>
+                  <p className="border-l min-w-[200px] text-center">31st March {currentYear-1}</p>
+
                 </div>
+                
               </div>
               <div>
-                <details className="">
-                  <summary  className="border w-full px-3 font-medium">
-                    Revenue From Operations
+                <details className="border-4  w-full">
+                  <summary className=" flex items-center justify-between w-full pl-3 cursor-pointer list-none">
+                    <span className="flex items-center gap-3">
+                      <span className="inline-block w-2 h-2 rotate-0 transition-transform details-arrow" />
+                      <span className="font-medium">Revenue From Operations</span>
+                    </span>
+                    <span className="flex">
+                      <span  className="border-l min-w-[200px] text-center pl-2 flex items-center justify-between px-2"><span>Total in Rs.</span> <strong>0/-</strong></span>
+                      <span className="border-l min-w-[200px] text-center flex items-center justify-between px-2"><span>Total in Rs.</span> <strong>0/-</strong></span>
+                    </span>
                   </summary>
                   <RevenueFromOperations/>
                 </details>
-                <details className="">
-                  <summary  className="border border-t-0 w-full px-3 font-medium">
-                    Other Income
+
+                <details className="border-4 border-t-0  w-full">
+                  <summary className=" flex items-center justify-between w-full pl-3 cursor-pointer list-none">
+                    <span className="flex items-center gap-3">
+                      <span className="inline-block w-2 h-2 rotate-0 transition-transform details-arrow" />
+                      <span className="font-medium">Other Income</span>
+                    </span>
+                    <span className="flex">
+                      <span  className="border-l min-w-[200px] text-center pl-2 flex items-center justify-between px-2"><span>Total in Rs.</span> <strong>0/-</strong></span>
+                      <span className="border-l min-w-[200px] text-center flex items-center justify-between px-2"><span>Total in Rs.</span> <strong>0/-</strong></span>
+                    </span>
                   </summary>
-                  <OtherIncome/>
+                  <OtherIncome />
                 </details>
+                
               </div>
             </div>
           }{
             rightContent === data?.BalanceSheet[4] && <div>
               <div  className="w-full">
-              <div className="font-bold border border-b-0 flex items-center justify-between">
-                <h1 className=" pl-2">{rightContent}</h1>
-                <div className="flex ">
-                  <p className="border-l  min-w-[250px] text-center pl-2 py-1.5" >31st March {currentYear}</p>
-                  <p className="border-l min-w-[250px] text-center">31st March {currentYear-1}</p>
+                <div className="font-bold border flex items-center justify-between">
+                  <h1 className=" pl-2">{rightContent}</h1>
                 </div>
               </div>
-              <div>
-                <details className="">
-                  <summary  className="border w-full px-3">
-                    Revenue From Operations
-                  </summary>
-                  <RevenueFromOperations/>
-                </details>
-              </div>
             </div>
-              
-              </div>
           }
           {
             rightContent === data?.BalanceSheet[5] && <div><h1 className="font-bold">{rightContent}</h1></div>
@@ -415,7 +426,7 @@ const EachClientData = () => {
             rightContent === data?.BalanceSheet[8] && <div><h1 className="font-bold">{rightContent}</h1></div>
           }
           
-        </div>
+        </div>}
       
       </div>
     </div>
